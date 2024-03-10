79dbf35d2464bb53bd049f9d3d2e1663fdaeebf4
adityabhatnagar29 <69914430+adityabhatnagar29@users.noreply.github.com>
2024-03-10 16:36:34 +0530

    Add files via upload
    This is SpringBoot Project

---
+++ src/main/java/com/demoApp/Controller/Controller.java
@@ -0,0 +1,15 @@
+package com.demoApp.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class Controller {
+
+	@GetMapping("/get")
+	public String getMethod() {
+		return "This is the Kubernetes and Docker Assignment.";
+	}
+}
